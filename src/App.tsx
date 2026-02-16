27e1cd56a2d9a9978efa8b00a5ee3896ae6923ea
karolcavalcante33-afk <karolcavalcante33@gmail.com>
2026-02-16 20:34:52 -0300

    Corrigindo props do ListaContatos

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
-import { useState } from 'react'
-import Formulario from './components/Formulario'
-import ListaContatos from './components/ListaContatos'
-import { Container, Title, GlobalStyle } from './styles/styled'
-
-type Contato = {
-  id: number
-  nome: string
-  email: string
-  telefone: string
-}
-
 function App() {
-  const [contatoParaEditar, setContatoParaEditar] = useState<Contato | null>(null)
+  const [contatoParaEditar, setContatoParaEditar] =
+    useState<Contato | null>(null)
 
   return (
     <>
@@ -27,10 +16,9 @@ function App() {
 
         <ListaContatos
           setContatoParaEditar={setContatoParaEditar}
+          contatoParaEditar={contatoParaEditar}
         />
       </Container>
     </>
   )
 }
-
-export default App
